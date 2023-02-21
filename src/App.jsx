fa74810513ea2038584cacfeefd512e7cd582122
Tomek <ttwitecki@gmail.com>
2023-02-21 21:32:47 +0100

    section component/baner

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-
+import Section from "./Components/Section/Section";
+import Banner from "./Ilustrations/BannerWhite.svg";
 function App() {
   return (
-    <div className="App">
-    <h1>Homework Case Study</h1>
-    </div>  
+    <div>
+      <div className="Banner_Container"></div>
+    {/* <img className="Banner" src={Banner}></img> */}
+    <div className="Main_Container">
+
+      <Section Title="Problem" Heading="Users often find mobile furniture 
+e-commerce convoluted and hard to use." Content="Mobile furniture e-commerce can be challenging for users because it often involves multiple steps and a lot of information to process. Customers may have difficulty navigating the website, finding the products they are looking for, and understanding the various options and features available. All of these factors can make the mobile furniture e-commerce experience frustrating and time-consuming for users."></Section>
+    <Section Title="Problem" Heading="Users often find mobile furniture 
+e-commerce convoluted and hard to use." Content="Mobile furniture e-commerce can be challenging for users because it often involves multiple steps and a lot of information to process. Customers may have difficulty navigating the website, finding the products they are looking for, and understanding the various options and features available. All of these factors can make the mobile furniture e-commerce experience frustrating and time-consuming for users."></Section>
+    </div>
+    </div>
   );
 }
 
---
+++ src/Components/Section/Section.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function Section(props) {
+
+    return (
+      <div className="Section_Container">
+        <hr></hr>
+        <span className="Section_Title">{props.Title}</span>
+        <span className="Section_Heading">{props.Heading}</span>
+        <span className="Section_Content">{props.Content}</span>
+        <hr></hr>
+      </div>
+    );
+  }
+
+
+
+export default Section;
